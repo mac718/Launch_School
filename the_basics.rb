deab8970c4adf56958eedf0b5367debb2c16cc97
mac718 <mac718@gmail.com>
2016-12-29 20:44:46 -0500

    Initial Commit

---
+++ the_basics.rb
@@ -0,0 +1,36 @@
+#1
+"Mike " + "Coon"
+
+#2
+number = 4321
+
+thousands = number / 1000
+hundreds = number % 1000 / 100 
+tens = number % 1000 % 100 / 10
+ones = number % 1000 % 100 % 10 
+
+#3
+movies = {the_big_lebowski: 1998, annie_hall: 1977}
+
+puts movies[:the_big_lebowski]
+puts movies[:annie_hall]
+
+#4
+dates = [1998, 1977]
+
+puts dates[0]
+puts dates[1]
+
+#5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#6
+puts 3.14**2
+puts 100.67**2
+puts 11.11**2
+
+#7
+#The error indicates that a closed parenthesis was used where a closed curly brakcet should have been used.
