4cccb8e126e9e1e988b8de7603c7fe675c0fdce9
mac718 <mac718@gmail.com>
2016-12-30 18:05:51 -0500

    Added arrays.rb

---
+++ arrays.rb
@@ -0,0 +1,33 @@
+#1
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+arr.include?(3)
+
+#2
+1. 1
+2. [1, 2, 3]
+
+#3
+puts arr[1][0]
+
+#4
+1. 3
+2. error
+3. 8
+
+#5
+1. c = 'e'
+2. c = 'A'
+3. c = nil
+
+#6
+The problem would be fixed by assigning the string 'jody' as the value of names[3], 
+where the value 'margaret' is currently stored.
+
+#7
+arr1 = [1, 2, 3, 4, 5]
+arr2 = arr1.map{|num| num + 2}
+
+p arr1
+p arr2
