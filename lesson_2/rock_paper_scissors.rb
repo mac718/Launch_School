c612d143d6c276a104085b014a042e2d205d507e
mac718 <mac718@gmail.com>
2017-01-07 22:35:43 -0500

    Added first-to-win-five-games functionality

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,26 +1,26 @@
-VALID_CHOICES = {'r' => 'rock',
-                 'p' => 'paper',
-                 'sc' => 'scissors',
-                 'sp' => 'spock',
-                 'l' => 'lizard'}
+VALID_CHOICES = { 'r' => 'rock',
+                  'p' => 'paper',
+                  'sc' => 'scissors',
+                  'sp' => 'spock',
+                  'l' => 'lizard' }
 
-WINNING_COMBOS = {'rock' => %w(scissors sc lizard l),
-                  'r' => %w(scissors sc lizard l),
-                  'paper' => %W(rock r spock sp),
-                  'p' => %W(rock r spock sp),
-                  'scissors' => %W(paper p lizard l),
-                  's' => %W(paper p lizard l),
-                  'spock' => %W(rock r scissors sc),
-                  'sp' => %W(rock r scissors sc),
-                  'lizard' => %W(spock sp paper p),
-                  'l' => %W(spock sp paper p)}
+WINNING_COMBOS = { 'rock' => %w(scissors sc lizard l),
+                   'r' => %w(scissors sc lizard l),
+                   'paper' => %w(rock r spock sp),
+                   'p' => %w(rock r spock sp),
+                   'scissors' => %w(paper p lizard l),
+                   's' => %w(paper p lizard l),
+                   'spock' => %w(rock r scissors sc),
+                   'sp' => %w(rock r scissors sc),
+                   'lizard' => %w(spock sp paper p),
+                   'l' => %w(spock sp paper p) }
 
 def prompt(message)
   puts("=> #{message}")
 end
 
 def win?(first, second)
-   WINNING_COMBOS[first].include?(second)
+  WINNING_COMBOS[first].include?(second)
 end
 
 def display_results(player, computer)
@@ -34,31 +34,60 @@ def display_results(player, computer)
 end
 
 loop do
-  choice = ''
-  loop do
-    prompt("Choose one: (r)rock, (p) paper, (sc)scissors, (sp)spock, (l)lizard")
-    choice = gets.chomp
+  player_wins = 0
+  computer_wins = 0
+  while player_wins < 5 && computer_wins < 5
 
-    if VALID_CHOICES.keys.include?(choice)
-      choice = VALID_CHOICES[choice]
-      break
-    elsif VALID_CHOICES.values.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
+    choice = ''
+
+    loop do
+      prompt("Choose one: (r)rock, (p) paper, (sc)scissors,
+            (sp)spock, (l)lizard")
+      choice = gets.chomp
+
+      if VALID_CHOICES.keys.include?(choice)
+        choice = VALID_CHOICES[choice]
+        break
+      elsif VALID_CHOICES.values.include?(choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
+
+    computer_choice = VALID_CHOICES.values.to_a.sample
+
+    prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+    display_results(choice, computer_choice)
+
+    if win?(choice, computer_choice)
+      player_wins += 1
+    elsif win?(computer_choice, choice)
+      computer_wins += 1
+    end
+
   end
 
-  computer_choice = VALID_CHOICES.values.to_a.sample
-
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-
-  display_results(choice, computer_choice)
+  if player_wins == 5
+    prompt("You beat computer! Awesome!") 
+  else
+    prompt("Computer won. Bummer!")
+  end
 
   prompt("Do you want to play again?")
   answer = gets.chomp
 
-  break unless answer.downcase.start_with?('y')
+  until answer.downcase == 'y' || answer.downcase == 'n'
+    prompt("Enter Y to play again or N to exit.")
+    answer = gets.chomp.strip
+  end
+
+  if answer.downcase == 'y'
+    next
+  elsif answer.downcase == 'n'
+    break
+  end
 end
 
 prompt("Thank you for playing. Good bye!")
