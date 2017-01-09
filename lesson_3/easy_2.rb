b1712f327742119475255415985c1775e495408f
mac718 <mac718@gmail.com>
2017-01-08 22:42:02 -0500

    Added second set of easy exercises

---
+++ lesson_3/easy_2.rb
@@ -0,0 +1,34 @@
+#Question 1
+ages.include?("Spot")
+ages.key?("Spot")
+ages.member?("Spot")
+
+#Question 2
+munster_description.capitalize!
+munster_description.swapcase!
+munsters_description.downcase!
+munsters_description.upcase!
+
+#Question 3
+ages.merge!(additional_ages)
+
+#Question 4
+advice.match("Dino")
+
+#Question 5
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+#Question 6
+flintstones.push("Dino")
+
+#Question 7
+flintstones.push("Dino").push("Hoppy")
+
+#Question 8
+advice.slice!(0, advice.index("house"))
+
+#Question 9
+statement.count('t')
+
+#Questtion 10
+title.center(40)
