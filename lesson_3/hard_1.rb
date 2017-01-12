4301185ee6f449bcb7a867cc8b56b90713934769
mac718 <mac718@gmail.com>
2017-01-11 21:01:54 -0500

    Solutions to hard exercises

---
+++ lesson_3/hard_1.rb
@@ -0,0 +1,32 @@
+#Question 1
+#greeting will be set to nil
+
+#Question 2
+#{a: 'hi there'}
+
+#Question 3
+#A)
+#one is: one
+#two is: two
+#three is: three
+#B)
+#one is: one
+#two is: two
+#three is: three
+#C)
+#one is: two
+#two is: three
+#three is: one
+
+#Question 4
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  if dot_separated_words.size != 4
+    false
+  else
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+  true
+end
