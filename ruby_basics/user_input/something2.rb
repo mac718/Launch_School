f3e85794664c6040b240fa06e45bae1ac3a7d271
mac718 <mac718@gmail.com>
2017-01-15 15:54:31 -0500

    Print Something (Part 2)

---
+++ ruby_basics/user_input/something2.rb
@@ -0,0 +1,11 @@
+answer = nil
+loop do
+  puts ">> Do you want me to print something? (y/n)"
+  answer = gets.chomp.strip
+  break if %(y n).include?(answer.downcase)
+  puts "Invalid input! Please enter a y or n."
+end 
+
+puts "something" if answer.downcase == 'y'
+
+
