c6d05dec41eab824de8c414d2a5a48b790d48f1d
Michael Coon <mac718@gmail.com>
2017-01-17 17:42:23 -0500

    Added Exercise 6

--- lesson_4/exercises:_nested_coll_and_blocks.rb
+++ lesson_4/exercises:_nested_coll_and_blocks.rb
@@ -40,3 +40,12 @@ arr.map do |sub_arr|
     b <=> a 
   end
 end
+
+#Exercise 6
+[{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}].map do |hash|
+  incremented_hash = {}
+  hash.map do |key, value|
+    incremented_hash[key] = value + 1
+  end
+ incremented_hash
+end
