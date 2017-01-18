6c9e195be716965f3dc8666e9d3a6e3308ad35f1
Michael Coon <mac718@gmail.com>
2017-01-18 18:23:02 -0500

    Added exercise 11

--- lesson_4/exercises:_nested_coll_and_blocks.rb
+++ lesson_4/exercises:_nested_coll_and_blocks.rb
@@ -94,3 +94,14 @@ hsh.map do |key, value|
     value[:size].upcase
   end
 end
+
+#Exercise 11
+arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
+
+arr.select do |hash|
+  hash.all? do |_, values|
+    values.all? do |num|
+      num.even?
+    end
+  end
+end
