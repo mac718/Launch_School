143800fad13a2a70644b394a5580fe396915c495
mac718 <mac718@gmail.com>
2017-01-21 21:57:44 -0500

    finished up exercises

--- ruby_basics/methods.rb
+++ ruby_basics/methods.rb
@@ -28,3 +28,55 @@ def car(make, model)
   puts "#{make} #{model}"
 end
 car('Toyota', 'Corolla')
+
+#Naming Animals
+def dog(name)
+  return name
+end
+
+def cat(name)
+  return name
+end
+
+puts "The dog's name is #{dog('Spot')}."
+puts "The cat's name is #{cat('Ginger')}."
+
+#Name Not Found
+def assign_name(name = "Bob")
+  name
+end
+
+#Multiply the Sum
+def add(num1, num2)
+  num1 + num2
+end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+#Random Sentence
+names = ['Dave', 'Sally', 'George', 'Jessica']
+activities = ['walking', 'running', 'cycling']
+
+def name(names)
+  names.sample
+end
+
+def activity(activities)
+  activities.sample
+end
+
+def sentence(name, activity)
+  "#{name} went #{activity} today!"
+end
+
+#Print Me (Part 1)
+def print_me
+  puts "I'm printing withing the method!"
+end
+
+#Print Me (Part 2)
+def print_me
+  "I'm printing withing the method!"
+end
