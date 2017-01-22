de1bcef86a041cb42b25abcd866be82b10fe9977
mac718 <mac718@gmail.com>
2017-01-22 10:33:19 -0500

    Rubocop refinements

--- lesson_5/tictactoe.rb
+++ lesson_5/tictactoe.rb
@@ -1,5 +1,8 @@
 require 'pry'
 
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
 INITIAL_MARKER = " "
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "O"
@@ -28,18 +31,18 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_place_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square #{empty_squares(brd).join(",")}:"
+    prompt "Choose a square #{empty_squares(brd).join(',')}:"
     square = gets.chomp.to_i
     if empty_squares(brd).include?(square)
       break
@@ -64,24 +67,17 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + #rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + #cols
-                  [[1, 5, 9], [3, 5, 7]]              #diagonals
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER && 
-       brd[line[1]] == PLAYER_MARKER && 
-       brd[line[2]] == PLAYER_MARKER
-       return "Player"
-     elsif brd[line[0]] == COMPUTER_MARKER && 
-       brd[line[1]] == COMPUTER_MARKER && 
-       brd[line[2]] == COMPUTER_MARKER
-       return "Computer"
-     end
-   end
-    nil 
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
 end
 
-loop do 
+loop do
   board = initialize_board
 
   loop do
