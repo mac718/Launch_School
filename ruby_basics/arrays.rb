3cb020f6d7efb4124cbcc20b73c4fd42fa99065e
mac718 <mac718@gmail.com>
2017-02-22 17:35:24 -0500

    completed exercises

---
+++ ruby_basics/arrays.rb
@@ -0,0 +1,59 @@
+#New Pet
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2]
+
+puts "I have a pet #{my_pet}!"
+
+#More than One
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+
+puts "I have a pet #{my_pet[0]} and #{my_pet[1]}!"
+
+#Free the Lizard
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+my_pets.pop
+
+puts "I have a pet #{my_pets[0]}!"
+
+#One isn't Enough
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+my_pets.pop
+
+my_pets << pets[1]
+
+puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
+
+#What Color Are You?
+colors = ['red', 'yellow', 'purple', 'green']
+
+colors.each { |color| puts "I'm the color #{color}!" }
+
+#Doubled
+numbers = [1, 2, 3, 4, 5]
+
+doubled_numbers = numbers.map { |number| number * 2 }
+
+p doubled_numbers
+
+#Divisible by Three
+numbers = [5, 9, 21, 26, 39]
+
+divisible_by_three = numbers.select { |num| num % 3 == 0 }
+p divisible_by_three
+
+#Favorite Number (Part 1)
+[['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+
+#Favorite Number (Part 2)
+favorites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+flat_favorites = favorites.flatten
+p flat_favorites
+
+#Are We the Same?
+array1 = [1, 5, 9]
+array2 = [1, 9, 5]
+
+puts array1 == array2
