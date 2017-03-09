a043c7962ab4c993283019cb8909f46d5cf18105
mac718 <mac718@gmail.com>
2017-03-08 23:04:13 -0500

    easy 3.2

---
+++ small_problems/easy_3/arithmetic_integer.rb
@@ -0,0 +1,12 @@
+puts '==> Enter the first number:'
+number1 = gets.chomp.strip.to_i
+
+puts '==> Enter the second number:'
+number2 = gets.chomp.strip.to_i
+
+puts "==> #{number1} + #{number2} = #{number1 + number2}"
+puts "==> #{number1} - #{number2} = #{number1 - number2}"
+puts "==> #{number1} * #{number2} = #{number1 * number2}"
+puts "==> #{number1} / #{number2} = #{number1 / number2}"
+puts "==> #{number1} % #{number2} = #{number1 % number2}"
+puts "==> #{number1} ** #{number2} = #{number1 ** number2}"
