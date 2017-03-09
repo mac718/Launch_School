6ff9d2b41869217b5c8588b586bb971519f2dee7
mac718 <mac718@gmail.com>
2017-03-09 16:53:19 -0500

    easy 3.5

--- small_problems/easy_3/squaring_an_argument.rb
+++ small_problems/easy_3/squaring_an_argument.rb
@@ -5,3 +5,13 @@ end
 def square(x)
   multiply(x, x)
 end
+
+def power(x, exp)
+  if exp == 1
+    multiply(x, 1) 
+  elsif exp == 2
+    multiply(x, x)
+  else
+    multiply(x, x) * x**(exp-2)
+  end
+end
