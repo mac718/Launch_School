d3204fa6c6f59770f142e84130a7e76bc0547699
mac718 <mac718@gmail.com>
2017-03-28 17:44:00 -0400

    easy 7.5

--- small_problems/easy_7/staggered_caps_p1.rb
+++ small_problems/easy_7/staggered_caps_p1.rb
@@ -12,22 +12,16 @@ end
 
 def staggered_case(str, first_char = true)
   staggered_str = ''
-  if first_char
+  if first_cap
     str.chars.each_with_index do |char, index|
-      if index.even?
-        staggered_str << char.upcase
-      else
-        staggered_str << char.downcase
-      end
-    end
+      staggered_str << char.upcase if index.even?
+      staggered_str << char.downcase if index.odd?
+    end 
   else
     str.chars.each_with_index do |char, index|
-      if index.odd?
-        staggered_str << char.upcase
-      else
-        staggered_str << char.downcase
-      end
-    end
+      staggered_str << char.downcase if index.even?
+      staggered_str << char.upcase if index.odd?
+    end 
   end
   staggered_str
 end 
