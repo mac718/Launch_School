9639fdaaa28218c0629034c1c8bf81ae29bd13b9
mac718 <mac718@gmail.com>
2017-03-30 21:29:36 -0400

    easy 8.4

---
+++ small_problems/easy_8/all_substrings.rb
@@ -0,0 +1,16 @@
+def substrings(str)
+  substring_arr = []
+  start_index = 0
+  loop do
+    end_index = start_index
+    loop do
+      p end_index
+      substring_arr << str[start_index..end_index]
+      end_index += 1
+      break if  end_index == str.length
+    end 
+    start_index += 1 
+    break if start_index == str.length 
+  end 
+  substring_arr
+end 
